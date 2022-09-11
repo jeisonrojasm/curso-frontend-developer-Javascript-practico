cf7ee9053e1440e15bfa151ad1543e6d55cf3b60
Jeison Rojas <j_esteban96@hotmail.com>
2022-09-10 22:24:46 -0500

    Con JS programo para que aparezca y desaparezca la ventana de detalle de producto con eventListener click

--- main.js
+++ main.js
@@ -3,12 +3,15 @@ const desktopMenu = document.querySelector(`.desktop-menu`);
 const menuHamIcon = document.querySelector(`.menu`);
 const mobileMenu = document.querySelector(`.mobile-menu`);
 const menuCarritoIcon = document.querySelector(`.navbar-shopping-cart`);
-const aside = document.querySelector(`.product-detail`);
+const shoppingCartContainer = document.querySelector(`#shoppingCartContainer`);
 const cardsContainer = document.querySelector(`.cards-container`);
+const productDetailContainer = document.querySelector(`#productDetail`);
+const productDetailCloseIcon = document.querySelector(`.product-detail-close`);
 
 menuEmail.addEventListener(`click`, toggleDesktopMenu);
 menuHamIcon.addEventListener(`click`, toggleMobileMenu);
 menuCarritoIcon.addEventListener(`click`, toggleCarritoAside);
+productDetailCloseIcon.addEventListener(`click`, closeProductDetailAside);
 
 function toggleDesktopMenu(event) {
     /*
@@ -18,36 +21,51 @@ function toggleDesktopMenu(event) {
         desktopMenu.classList.add(`inactive`);
     }
     */
-    const isAsideClosed = aside.classList.contains(`inactive`);
+    const isAsideClosed = shoppingCartContainer.classList.contains(`inactive`);
 
     if (!isAsideClosed) {
-        aside.classList.add(`inactive`);
+        shoppingCartContainer.classList.add(`inactive`);
     }
 
     desktopMenu.classList.toggle(`inactive`);
 }
 
 function toggleMobileMenu(event) {
-    const isAsideClosed = aside.classList.contains(`inactive`);
+    const isAsideClosed = shoppingCartContainer.classList.contains(`inactive`);
 
     if (!isAsideClosed) {
-        aside.classList.add(`inactive`);
+        shoppingCartContainer.classList.add(`inactive`);
     }
+
+    closeProductDetailAside();
 
     mobileMenu.classList.toggle(`inactive`);
 }
 
 function toggleCarritoAside(event) {
     const isMobileMenuClosed = mobileMenu.classList.contains(`inactive`);
-    const isDesktopMenuClosed = desktopMenu.classList.contains(`inactive`);
 
     if (!isMobileMenuClosed) {
         mobileMenu.classList.add(`inactive`);
-    } else if (!isDesktopMenuClosed) {
-        desktopMenu.classList.add(`inactive`);
     }
 
-    aside.classList.toggle(`inactive`);
+    const isProductDetailClosed = productDetailContainer.classList.contains(`inactive`);
+
+    if (!isProductDetailClosed) {
+        productDetailContainer.classList.add(`inactive`);
+    }
+
+    shoppingCartContainer.classList.toggle(`inactive`);
+}
+
+function openProductDetailAside(event) {
+    shoppingCartContainer.classList.add(`inactive`);
+
+    productDetailContainer.classList.remove(`inactive`);
+}
+
+function closeProductDetailAside(event) {
+    productDetailContainer.classList.add(`inactive`);
 }
 
 const productList = [];
@@ -86,6 +104,7 @@ function renderProducts(arr) {
 
         const productImg = document.createElement(`img`);
         productImg.setAttribute(`src`, product.image);
+        productImg.addEventListener(`click`, openProductDetailAside);
 
         const productInfo = document.createElement(`div`);
         productInfo.classList.add(`product-info`);
