765c4f228eafc14db07c4d03eb72d81913d0c3f3
Jeison Rojas <j_esteban96@hotmail.com>
2022-09-10 20:46:20 -0500

    Programo con JS para que los productos aparezcan haciendo uso de ciclo for of

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuHamIcon = document.querySelector(`.menu`);
 const mobileMenu = document.querySelector(`.mobile-menu`);
 const menuCarritoIcon = document.querySelector(`.navbar-shopping-cart`);
 const aside = document.querySelector(`.product-detail`);
+const cardsContainer = document.querySelector(`.cards-container`);
 
 menuEmail.addEventListener(`click`, toggleDesktopMenu);
 menuHamIcon.addEventListener(`click`, toggleMobileMenu);
@@ -47,4 +48,67 @@ function toggleCarritoAside(event) {
     }
 
     aside.classList.toggle(`inactive`);
+}
+
+const productList = [];
+
+function creaProducto(name, price, image) {
+    this.name = name;
+    this.price = price;
+    this.image = image;
+}
+
+const bike1 = new creaProducto(`Bike`, 120, `https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940`);
+
+const pantalla1 = new creaProducto(`Pantalla`, 220, `https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940`);
+
+const computador1 = new creaProducto(`Compu`, 620, `https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940`);
+
+const bike2 = new creaProducto(`Bike`, 120, `https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940`);
+
+const pantalla2 = new creaProducto(`Pantalla`, 220, `https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940`);
+
+const computador2 = new creaProducto(`Compu`, 620, `https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940`);
+
+productList.push(bike1);
+productList.push(pantalla1);
+productList.push(computador1);
+productList.push(bike2);
+productList.push(pantalla2);
+productList.push(computador2);
+
+renderProducts(productList);
+
+function renderProducts(arr) {
+    for (const product of arr) {
+        const productCard = document.createElement(`div`);
+        productCard.classList.add(`product-card`);
+
+        const productImg = document.createElement(`img`);
+        productImg.setAttribute(`src`, product.image);
+
+        const productInfo = document.createElement(`div`);
+        productInfo.classList.add(`product-info`);
+
+        const productInfoDiv = document.createElement(`div`);
+        const productPrice = document.createElement(`p`);
+        productPrice.innerText = `$ ${product.price}`;
+        const productName = document.createElement(`p`);
+        productName.innerText = product.name;
+
+        productInfoDiv.append(productPrice, productName);
+
+
+        const productInfoFigure = document.createElement(`figure`);
+        const productImgCart = document.createElement(`img`);
+        productImgCart.setAttribute(`src`, `./icons/bt_add_to_cart.svg`);
+
+        productInfoFigure.appendChild(productImgCart);
+
+        productInfo.append(productInfoDiv, productInfoFigure);
+
+        productCard.append(productImg, productInfo);
+
+        cardsContainer.appendChild(productCard);
+    }
 }
